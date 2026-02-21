4aa74dacf2bd43ed6bc742001459e770ff31e29e
Musharraf-Bubere <musharrafbubere007@gmail.com>
2026-02-21 15:51:34 +0530

    updated

--- streamlit_app/ui.py
+++ streamlit_app/ui.py
@@ -9,14 +9,14 @@ st.title("🌦 Weather Thunderstorm Prediction App")
 st.write("Enter atmospheric parameters to predict TH (Thunderstorm Occurrence)")
 
 # Input fields
-SWEAT_index = st.number_input("SWEAT Index")
-K_index = st.number_input("K Index")
-Totals_totals_index = st.number_input("Totals Totals Index")
-Environmental_Stability = st.number_input("Environmental Stability")
-Moisture_Indices = st.number_input("Moisture Indices")
-Convective_Potential = st.number_input("Convective Potential")
-Temperature_Pressure = st.number_input("Temperature Pressure")
-Moisture_Temperature_Profiles = st.number_input("Moisture Temperature Profiles")
+SWEAT_index = st.number_input("SWEAT index")
+K_index = st.number_input("K index")
+Totals_totals_index = st.number_input("Totals totals Index")
+Environmental_Stability = st.number_input("Environmental_Stability")
+Moisture_Indices = st.number_input("Moisture_Indices")
+Convective_Potential = st.number_input("Convective_Potential")
+Temperature_Pressure = st.number_input("Temperature_Pressure")
+Moisture_Temperature_Profiles = st.number_input("Moisture_Temperature_Profiles")
 
 if st.button("Predict"):
     payload = {
